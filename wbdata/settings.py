1716913766cfc6aafc8873e3d977a1a18f64a152
Garvit Singla <singlagarvit68@gmail.com>
2021-06-18 02:19:35 +0530

    Email Update

--- wbdata/settings.py
+++ wbdata/settings.py
@@ -64,7 +64,16 @@ TEMPLATES = [
         },
     },
 ]
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtpout.secureserver.net'
+EMAIL_HOST_USER = 'info@wbbsedata.com'
+EMAIL_HOST_PASSWORD = 'Subhro@123'
+EMAIL_PORT = 80
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+DEFAULT_FROM_EMAIL = 'info@wbbsedata.com'
+
 WSGI_APPLICATION = 'wbdata.wsgi.application'
 
 
