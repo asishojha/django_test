24762e799df47bf887a4552d7d38e1682d692655
Garvit Singla <singlagarvit68@gmail.com>
2021-06-18 14:49:04 +0530

    Correct Settings.py

--- wbdata/settings.py
+++ wbdata/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'wbdata.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'mydb',
-#         'USER': 'idms',
-#         'PASSWORD': 'Idms1994',
-#         'HOST': '20.204.49.50',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'idms',
+        'PASSWORD': 'Idms1994',
+        'HOST': '20.204.49.50',
+        'PORT': '5432',
+    }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
